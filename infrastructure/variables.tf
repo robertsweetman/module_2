5ee47f4276df782b13bb1d5b6da962aeca404795
robertsweetman <robertsweetman@om.me>
2025-05-25 19:26:52 +0100

    tf state backend bucket

---
+++ infrastructure/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+    type = string
+    default = "eu-west-2"
+}
+
+# TODO: look this up after it's been created instead rather than hardcoding?
+variable "tf_state_bucket_name" {
+    type = string
+    default = "tfstate"
+}
