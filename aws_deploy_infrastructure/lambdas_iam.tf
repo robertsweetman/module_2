e6159c83f92f9ff9e5a1c3c3aebc57e7a9a03b33
robertsweetman <robertsweetman@om.me>
2025-06-07 18:21:22 +0100

    add missing lambda iam

---
+++ aws_deploy_infrastructure/lambdas_iam.tf
@@ -0,0 +1,79 @@
+# Lambda execution role
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach policies to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# RDS access policy
+resource "aws_iam_role_policy" "lambda_rds_access" {
+  name = "lambda_rds_access"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "rds:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Step Functions access for postgres_dataload Lambda
+resource "aws_iam_role_policy" "lambda_sfn_access" {
+  name = "lambda_sfn_access"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "states:StartExecution"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_s3_access" {
+    name = "lambdas_s3_access"
+    role = aws_iam_role.lambda_role.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "s3:*"
+                ]
+                Resource = "*"
+            }
+        ]
+    })
+}
