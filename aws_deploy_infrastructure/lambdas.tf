fdf26c0ab1ecdff245d63b6af2d14907b85a1438
robertsweetman <robertsweetman@om.me>
2025-06-08 20:12:33 +0100

    comment in lambdas to deploy

--- aws_deploy_infrastructure/lambdas.tf
+++ aws_deploy_infrastructure/lambdas.tf
@@ -1,48 +1,48 @@
-# resource "aws_lambda_function" "pdf_processing" {
-#   function_name = "pdf-processing"
-#   handler       = "bootstrap"
-#   runtime       = "provided.al2"
-#   role          = aws_iam_role.lambda_role.arn
+resource "aws_lambda_function" "pdf_processing" {
+  function_name = "pdf-processing"
+  handler       = "bootstrap"
+  runtime       = "provided.al2"
+  role          = aws_iam_role.lambda_role.arn
   
-#   s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
-#   s3_key        = "pdf_processing.zip"
+  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
+  s3_key        = "pdf_processing.zip"
 
-#   depends_on = [aws_s3_bucket.lambda_bucket]
-#   lifecycle {
-#     ignore_changes = [source_code_hash]
-#   }
+  depends_on = [aws_s3_bucket.lambda_bucket]
+  lifecycle {
+    ignore_changes = [source_code_hash]
+  }
   
-#   environment {
-#     variables = {
-#       DATABASE_URL = "postgres://${var.db_admin_name}:${var.db_admin_pwd}@${aws_db_instance.postgres.endpoint}/${var.db_name}"
-#     }
-#   }
+  environment {
+    variables = {
+      DATABASE_URL = "postgres://${var.db_admin_name}:${var.db_admin_pwd}@${aws_db_instance.postgres.endpoint}/${var.db_name}"
+    }
+  }
   
-#   timeout = 900
-#   memory_size = 1024
-# }
+  timeout = 900
+  memory_size = 1024
+}
 
-# resource "aws_lambda_function" "postgres_dataload" {
-#   function_name = "postgres-dataload"
-#   handler       = "bootstrap"
-#   runtime       = "provided.al2"
-#   role          = aws_iam_role.lambda_role.arn
+resource "aws_lambda_function" "postgres_dataload" {
+  function_name = "postgres-dataload"
+  handler       = "bootstrap"
+  runtime       = "provided.al2"
+  role          = aws_iam_role.lambda_role.arn
   
-#   s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
-#   s3_key        = "postgres_dataload.zip"
+  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
+  s3_key        = "postgres_dataload.zip"
 
-#   depends_on = [aws_s3_bucket.lambda_bucket]
-#   lifecycle {
-#     ignore_changes = [source_code_hash]
-#   }
+  depends_on = [aws_s3_bucket.lambda_bucket]
+  lifecycle {
+    ignore_changes = [source_code_hash]
+  }
   
-#   environment {
-#     variables = {
-#       DATABASE_URL = "postgres://${var.db_admin_name}:${var.db_admin_pwd}@${aws_db_instance.postgres.endpoint}/${var.db_name}"
-#       PDF_PROCESSING_STEP_FUNCTION_ARN = aws_sfn_state_machine.pdf_processing_workflow.arn
-#     }
-#   }
+  environment {
+    variables = {
+      DATABASE_URL = "postgres://${var.db_admin_name}:${var.db_admin_pwd}@${aws_db_instance.postgres.endpoint}/${var.db_name}"
+      PDF_PROCESSING_STEP_FUNCTION_ARN = aws_sfn_state_machine.pdf_processing_workflow.arn
+    }
+  }
   
-#   timeout = 900
-#   memory_size = 1024
-# }
+  timeout = 900
+  memory_size = 1024
+}
--- aws_deploy_infrastructure/outputs.tf
+++ aws_deploy_infrastructure/outputs.tf
@@ -9,10 +9,10 @@ output "db_name" {
   value = aws_db_instance.postgres.db_name
 }
 
-# output "pdf_processing_step_function_arn" {
-#   description = "ARN of the PDF processing Step Function workflow"
-#   value       = aws_sfn_state_machine.pdf_processing_workflow.arn
-# }
+output "pdf_processing_step_function_arn" {
+  description = "ARN of the PDF processing Step Function workflow"
+  value       = aws_sfn_state_machine.pdf_processing_workflow.arn
+}
 
 output "lambda_bucket_name" {
   description = "Name of the S3 bucket for Lambda deployment packages"
