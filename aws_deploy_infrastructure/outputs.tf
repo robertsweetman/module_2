b00f823d40916f657d64a23c5d189dfdbfb3351a
robertsweetman <robertsweetman@om.me>
2025-06-08 17:15:42 +0100

    add missing outputs

--- aws_deploy_infrastructure/outputs.tf
+++ aws_deploy_infrastructure/outputs.tf
@@ -6,3 +6,13 @@ output "db_endpoint" {
 output "db_name" {
   value = aws_db_instance.postgres.db_name
 }
+
+output "pdf_processing_step_function_arn" {
+  description = "ARN of the PDF processing Step Function workflow"
+  value       = aws_sfn_state_machine.pdf_processing_workflow.arn
+}
+
+output "lambda_bucket_name" {
+  description = "Name of the S3 bucket for Lambda deployment packages"
+  value       = aws_s3_bucket.lambda_bucket.bucket
+}
